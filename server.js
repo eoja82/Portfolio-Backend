960f17c0c5fa551b604e5e28f2d492f705672120
Erik Oja <erik_oja@yahoo.com>
2022-08-02 14:20:16 -0500

    add post route

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000
 
-// create application/json parser
-var jsonParser = bodyParser.json()
+app.use( bodyParser.json())
+
+app.route(`/${process.env.API_URL}`)
+  .post(function(req, res) {
+    console.log("Route Hit")
+    const email = req.body.email,
+          subject = req.body.subject,
+          message = req.body.message
+    console.log(email, subject, message)
+    res.json({email: email, message: message})
+  })
 
 //404 Not Found Middleware
 app.use(function(req, res, next) {
