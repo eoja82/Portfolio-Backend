0da30df15780289fcbf189b4b558258b936e9a65
Erik Oja <erik_oja@yahoo.com>
2022-08-05 16:17:49 -0500

    add contact route to handle sending email

--- server.js
+++ server.js
@@ -1,20 +1,56 @@
 require('dotenv').config()
 const bodyParser = require("body-parser")
+const cors = require("cors")
 const express = require("express")
+const nodemailer = require("nodemailer")
+//const helmet = require("helmet")
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use( bodyParser.json())
 
-app.route(`/${process.env.API_URL}`)
+app.use(bodyParser.json())
+//app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(cors())
+
+
+app.route("/contact")
   .post(function(req, res) {
-    console.log("Route Hit")
     const email = req.body.email,
+          name = req.body.name,
           subject = req.body.subject,
           message = req.body.message
-    console.log(email, subject, message)
-    res.json({email: email, message: message})
+    
+    if (email && name && subject && message) {
+      const transporter = nodemailer.createTransport({
+        service: process.env.EMAIL_SERVICE,
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.EMAIL_PASS
+        }
+      })
+      const mailOptions = {
+        from: process.env.EMAIL,
+        to: process.env.EMAIL,
+        subject: subject,
+        html: `
+          <p><b>Message:</b> ${message}</p>
+          <p><b>Name:</b> ${name}</p>
+          <p><b>Email:</b> ${email}</p>
+        `
+      }
+      transporter.sendMail(mailOptions, function(err, info) {
+        if (err) {
+          console.log(err)
+          res.json({message: "Sorry, something went wrong.  Please try again."})
+        } else {
+          res.json({message: "Thanks for getting in touch.  Your message has been sent.", success: "true"})
+        }
+      })
+    } else {
+      res.json({message: "Please fill out all fields"})
+    }
   })
 
 //404 Not Found Middleware
